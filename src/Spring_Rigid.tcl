06903eef8ddf0b123e62494acd9454594c441cc4
Ahmed Elkady <a.elkady@soton.ac.uk>
2023-01-03 08:39:40 +0000

    - src removed for yearly update

--- src/Spring_Rigid.tcl
+++
@@ -1,7 +0,0 @@
-
-proc Spring_Rigid {SpringID Node_i Node_j} {
-
-element zeroLength $SpringID  $Node_i $Node_j -mat 99 99 99 -dir 1 2 6 -doRayleigh 1;;
-
-
-}
--- src/Spring_Zero.tcl
+++
@@ -1,8 +0,0 @@
-
-proc Spring_Zero {SpringID Node_i Node_j} {
-
-element zeroLength $SpringID  $Node_i $Node_j -mat 9 -dir 6;
-
-equalDOF    $Node_i     $Node_j    1     2;
-
-}
